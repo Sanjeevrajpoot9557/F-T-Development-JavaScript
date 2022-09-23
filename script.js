d94c90f6267f6322f5e9782c3032dbd30f4c3d36
Sanjeev Kumar rajpoot <110648331+Sanjeevrajpoot9557@users.noreply.github.com>
2022-09-23 22:11:59 +0530

    Js Assignment
    all assignment solution with practice set

---
+++ script.js
@@ -0,0 +1,22 @@
+document.body.style.backgroundColor = "yellow";
+document.getElementById("p01").innerHTML = "Hellow sanjeev how are you" // this content is changing by me.
+document.getElementById('p1').style.color = 'black';
+document.getElementById('intro').style.fontSize = '50px';
+
+document.getElementById('p01').style.fontSize = '30px';
+
+document.getElementById('p01').style.fontWeight = 'bold' // this style add extra....
+
+
+
+document.getElementById("p1").innerHTML = "Java script is a power ful ,flexible, and fast programing language";
+
+document.getElementById("p2").innerHTML = "java script powers the dynamic behaivor on website.";
+
+document.getElementById("p3").innerHTML = "java script remains at the core web development.";
+
+document.getElementById("p1").style.color = 'red';
+
+document.getElementById('p2').style.color = 'blue';
+
+document.getElementById('p3').style.color = 'green';
